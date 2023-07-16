e21502867e6aa462d9b62a9a303bc055f7da7aed
Shubham Maharaj <shubhammaharaj@Shubhams-MacBook-Air.local>
2023-07-17 00:48:51 +0530

    firstapp

---
+++ AppPro.tsx
@@ -0,0 +1,19 @@
+import { View, Text ,StyleSheet, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+
+const AppPro = () => {
+  return (
+    <SafeAreaView>
+        <ScrollView>
+         
+          <Text>AppPro</Text>
+
+        </ScrollView>
+    </SafeAreaView>
+   
+   
+  
+  )
+}
+
+export default AppPro;
